a5cbe6ed829d95de71a0d3cf164058dca518ead0
Hrishit Dey <hrishitdey@gmail.com>
2026-05-08 21:47:49 +0530

    Do the following database administration commands: Create user, create role, grant privileges to a role, revoke privileges from a role, create index

---
+++ practical_2.sql
@@ -0,0 +1,26 @@
+-- create user
+CREATE USER IF NOT EXISTS 'demo'@'localhost'
+IDENTIFIED BY 'StrongPassword!123'
+PASSWORD EXPIRE INTERVAL 90 DAY
+PASSWORD HISTORY 5
+PASSWORD REUSE INTERVAL 365 DAY;
+SHOW CREATE USER 'demo'@'localhost';
+-- create role
+CREATE ROLE invoice_entry;
+SELECT user, host
+FROM mysql.user
+WHERE user='invoice_entry';
+-- grant privileges to a role
+GRANT INSERT, UPDATE
+ON EMPLOYEE
+TO 'invoice_entry';
+SHOW GRANTS FOR 'invoice_entry';
+-- revoke privileges from a role
+REVOKE INSERT, UPDATE
+ON EMPLOYEE
+TO 'invoice_entry';
+SHOW GRANTS FOR 'invoice_entry';
+-- create index
+CREATE INDEX idx_lname_dno
+ON EMPLOYEE(Lname, Dno);
+SHOW INDEX FROM EMPLOYEE;
